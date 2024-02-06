caaa3a5e9d525da1eec0d14f0121c5f3fa3cd4fc
vandodev <vandoaparecido@hotmail.com>
2024-02-06 09:41:00 -0300

    :hamburger: :fries: :fork_and_knife: Criando componente Header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,9 @@
+import { Header } from "@/components/header"
 import {View, Text} from "react-native"
 export default function Home(){
     return(
         <View className="bg-slate-900 pt-8 flex-1">
-            <Text className="text-white text-2xl font-heading">Olá mundo</Text>
+            <Header /> 
         </View>
     )
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import { Image, Text, View, TouchableOpacity } from 'react-native';
+export function Header(){
+    return (
+       <View className="flex-row items-center border-b border-slate-700 pb-5 mx-5">
+            <View className="flex-1">
+                <Image source={require('@/assets/logo.png')} className="h-6 w-32" />
+                <Text className="text-white text-xl font-heading mt-2">Cardápio</Text>
+            </View>
+       </View>
+    )
+}
