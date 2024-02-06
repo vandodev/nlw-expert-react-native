125f0c885b154a3e5a9e7429e62b08925dbe52ad
vandodev <vandoaparecido@hotmail.com>
2024-02-06 08:27:41 -0300

    :hamburger: :fries: :fork_and_knife: Estilizando com StyleSheet

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,15 @@
-import {View, Text} from "react-native"
+import {View, Text, StyleSheet} from "react-native"
 export default function Home(){
     return(
-        <View>
+        <View style={styles.container}>
             <Text>Olá mundo</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:"red"
+    }
+})
