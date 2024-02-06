2f4f973c72b8f2c20b8403a12e378b0858a9ed4d
vandodev <vandoaparecido@hotmail.com>
2024-02-06 09:15:48 -0300

    :hamburger: :fries: :fork_and_knife: utilizando layout slot e fontes personalizadas

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,8 @@
 import {View, Text} from "react-native"
 export default function Home(){
     return(
-        <View className="bg-slate-900 flex-1">
-            <Text className="text-white">Olá mundo</Text>
+        <View className="bg-slate-900 pt-8 flex-1">
+            <Text className="text-white text-2xl">Olá mundo</Text>
         </View>
     )
 }
