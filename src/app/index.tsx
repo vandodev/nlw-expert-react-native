60cf1533ebb7812b3238993733cd87b00ef94b6a
vandodev <vandoaparecido@hotmail.com>
2024-02-07 07:59:40 -0300

    :hamburger: :fries: :fork_and_knife: Icone de carrinho de compras

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import {View, Text} from "react-native"
 export default function Home(){
     return(
         <View className="bg-slate-900 pt-8 flex-1">
-            <Header title="Faça seu pedido" /> 
+            <Header title="Faça seu pedido" cartQuantityItems={5}/> 
         </View>
     )
 }
