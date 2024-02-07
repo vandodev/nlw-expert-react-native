34460a08ab2b0c8f60837f581de6778041575467
vandodev <vandoaparecido@hotmail.com>
2024-02-07 08:29:01 -0300

    :hamburger: :fries: :fork_and_knife: Criando componente lista de categorias

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,18 @@
+import { CategoryButton } from "@/components/category-button"
 import { Header } from "@/components/header"
 import {View, Text} from "react-native"
 export default function Home(){
     return(
         <View className="bg-slate-900 pt-8 flex-1">
             <Header title="Faça seu pedido" cartQuantityItems={5}/> 
+
+            <View className="flex-row gap-4">
+                 <CategoryButton title="Lanche do dia" isSelected/>
+                 <CategoryButton title="Promoções"/>
+                 <CategoryButton title="Sobremesa"/>
+                 <CategoryButton title="Bebidas"/>
+            </View>
+           
         </View>
     )
 }
