70e76c1e0221dbdb438db351eae9b11b3a46da81
vandodev <vandoaparecido@hotmail.com>
2024-02-07 08:42:20 -0300

    :hamburger: :fries: :fork_and_knife: Carregando catégorias de forma dinâmica

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,27 @@
 import { CategoryButton } from "@/components/category-button"
 import { Header } from "@/components/header"
-import {View, Text} from "react-native"
+import {View, FlatList} from "react-native"
+import { CATEGORIES} from '@/utils/data/products';
+
 export default function Home(){
     return(
         <View className="bg-slate-900 pt-8 flex-1">
             <Header title="Faça seu pedido" cartQuantityItems={5}/> 
 
-            <View className="flex-row gap-4">
-                 <CategoryButton title="Lanche do dia" isSelected/>
-                 <CategoryButton title="Promoções"/>
-                 <CategoryButton title="Sobremesa"/>
-                 <CategoryButton title="Bebidas"/>
-            </View>
+        <FlatList
+            data={CATEGORIES}
+            keyExtractor={(item) => item}
+            renderItem={({ item }) => (
+            <CategoryButton
+                title={item}
+            />
+            )}
+            horizontal
+            className="max-h-10 mt-5"
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+        />
+
            
         </View>
     )
