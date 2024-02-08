9c6edaa8e3a27a1acf17671fc01648d2cef2422b
vandodev <vandoaparecido@hotmail.com>
2024-02-08 10:02:18 -0300

    :hamburger: :fries: :fork_and_knife: Recurso composição de componente

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -2,6 +2,9 @@ import { Image, Text, View } from 'react-native';
 import { useLocalSearchParams, useNavigation } from 'expo-router';
 import { PRODUCTS } from '@/utils/data/products';
 import { formatCurrency } from '@/utils/functions/format-currency';
+import { Button } from '@/components/button';
+import { Feather } from '@expo/vector-icons';
+
 
 
 export default function Product() {
@@ -23,7 +26,21 @@ export default function Product() {
                     </Text>
                 ))}
 
-            </View>            
+            </View>  
+
+            <View className="p-5 pb-8 gap-5">
+                <Button>
+                    <Button.Icon>
+                        <Feather name="plus-circle" size={20} />
+                    </Button.Icon>
+
+                    <Button.Text>
+                        Adicionar ao pedido
+                    </Button.Text>
+                </Button>
+{/* 
+                <LinkButton title="Voltar ao cardápio" href="/" /> */}
+            </View>          
 
         </View>
         
