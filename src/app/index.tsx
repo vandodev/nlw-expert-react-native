f1d3222413ffc7eec9f359ff3a25578ef2fee64f
vandodev <vandoaparecido@hotmail.com>
2024-02-09 08:17:44 -0300

    :hamburger: :fries: :fork_and_knife: Ajuste de tipagem

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { useState, useRef } from 'react';
 import { CategoryButton } from "@/components/category-button"
 import { Header } from "@/components/header"
 import {View,Text, FlatList, SectionList} from "react-native"
-import { CATEGORIES, MENU} from '@/utils/data/products';
+import { CATEGORIES, MENU, ProductProps} from '@/utils/data/products';
 import { Link } from 'expo-router';
 import { Product } from '@/components/product';
 import { useCartStore } from '@/stores/cart-store';
@@ -10,7 +10,7 @@ import { useCartStore } from '@/stores/cart-store';
 export default function Home(){
     const cartStore = useCartStore();
     const [category, setCategory] = useState(CATEGORIES[0]);
-    const sectionListRef = useRef<SectionList>(null);
+    const sectionListRef = useRef<SectionList<ProductProps>>(null);
 
     const cartQuantityItems = cartStore.products.reduce(
         (total, product) => total + product.quantity,
@@ -58,7 +58,7 @@ export default function Home(){
         <SectionList
             ref={sectionListRef}
             sections={MENU}
-            keyExtractor={(item) => item.id}
+            keyExtractor={(item) => item.id} //facilita tipagem
             stickySectionHeadersEnabled={false}
             renderItem={({ item }) => (
                 <Link href={`/product/${item.id}`} asChild>
