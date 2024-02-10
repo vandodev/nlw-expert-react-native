8449bc6253295d20f8cec5361a73080a2b27d1e7
vandodev <vandoaparecido@hotmail.com>
2024-02-10 07:26:41 -0300

    :hamburger: :fries: :fork_and_knife: Simulando carrinho vazio

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,5 +1,5 @@
 import { Header } from "@/components/header";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 import { Product } from '@/components/product';
 import { ProductCartProps, useCartStore } from '@/stores/cart-store';
@@ -9,16 +9,26 @@ export default function Cart() {
     const cartStore = useCartStore();
     return(
         <View className="flex-1 mt-8">
-            <Header title="Seu carrinho" />
-
-            <View className="p-5 flex-1">         
+            <Header title="Seu carrinho" />        
+            
+          <View className="p-5 flex-1">
+            {cartStore.products.length > 0 ? (
+              <View className="border-b border-slate-700">
                 {cartStore.products.map((product) => (
                   <Product
                     key={product.id}
                     data={product}
                   />
                 ))}
-            </View>
+              </View>
+            ) : (
+              <Text className="font-body text-slate-400 text-center my-8">
+                Seu carrinho está vazio.
+              </Text>
+            )}
+                      
+          </View>      
+
         </View>
     )
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -36,7 +36,7 @@ export default function Home(){
 
     return(
         <View className="bg-slate-900 pt-8 flex-1">
-            <Header title="Faça seu pedido" cartQuantityItems={cartQuantityItems} /> 
+            <Header title="Faça seu pedido" cartQuantityItems={1} /> 
 
         <FlatList
             data={CATEGORIES}
