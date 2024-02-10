a2c14010eddd19280e3c4a89189a7860945efa2b
vandodev <vandoaparecido@hotmail.com>
2024-02-10 07:03:48 -0300

    :hamburger: :fries: :fork_and_knife: Exibindo produtos do carrinho

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,10 +1,24 @@
 import { Header } from "@/components/header";
 import { View } from "react-native";
 
+import { Product } from '@/components/product';
+import { ProductCartProps, useCartStore } from '@/stores/cart-store';
+
+
 export default function Cart() {
+    const cartStore = useCartStore();
     return(
         <View className="flex-1 mt-8">
             <Header title="Seu carrinho" />
+
+            <View className="p-5 flex-1">         
+                {cartStore.products.map((product) => (
+                  <Product
+                    key={product.id}
+                    data={product}
+                  />
+                ))}
+            </View>
         </View>
     )
 }
