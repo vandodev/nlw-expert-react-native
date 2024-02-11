4bb408e2480dc8bcc1a8b9d41ea38b2051d8e2ab
vandodev <vandoaparecido@hotmail.com>
2024-02-11 07:06:32 -0300

    :hamburger: :fries: :fork_and_knife: Persistindo dados com AsyncStorage

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,6 +1,8 @@
 import { create } from 'zustand';
-import { createJSONStorage, persist } from 'zustand/middleware';
 import * as cartInMemory from './helpers/cart-in-memory';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 import { ProductProps } from '@/utils/data/products';
 
@@ -14,7 +16,9 @@ type StateProps = {
   remove: (productId: string) => void;
 };
 
-export const useCartStore = create<StateProps>((set) => ({
+export const useCartStore = create(
+  persist<StateProps>(
+    (set) => ({
       products: [],
 
       add: (product: ProductProps) =>
@@ -22,9 +26,16 @@ export const useCartStore = create<StateProps>((set) => ({
           products: cartInMemory.add(state.products, product),
         })),
 
-        remove: (productId: string) =>
+      remove: (productId: string) =>
         set((state) => ({
           products: cartInMemory.remove(state.products, productId),
         })),
 
-}));
+      clear: () => set({ products: [] }),
+    }),
+    {
+      name: 'nlw-expert:cart',
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
